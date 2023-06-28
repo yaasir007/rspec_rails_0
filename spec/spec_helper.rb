ddc59f6ad9cad9ed1a0a22cebdc74031899bb7e6
Yaasir Cheekoory <yaasir1997@gmail.com>
2023-06-29 00:32:51 +0400

    test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,11 @@
+class Car
+    attr_reader :brand
+
+    def initialize(brand)
+        @brand = brand
+    end
+end
+
 RSpec.describe Car do
     it 'has a brand' do
         car = Car.new('Ford')
